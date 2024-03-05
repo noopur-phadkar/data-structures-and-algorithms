92123105413e9d4ca63b01d96b4a9db2e2c2dd5b
noopur-phadkar <np7541@rit.edu>
2024-03-04 22:05:35 -0500

    Adding insert into binary tree

--- binary-tree/main.py
+++ binary-tree/main.py
@@ -1,50 +1,54 @@
 class TreeNode:
-    def __init__(self, left_node, val, right_node):
-        self.left_node = left_node
-        self.val = val
-        self.right_node = right_node
-
-    def is_leaf(self, node):
-        if node.left_node is None and node.right_node is None:
-            return True
-        else:
-            return False
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
 
 
 class Tree:
     def __init__(self):
-        self.head = None
+        self.root = None
 
-    def create_tree(self, val):
-        self.head = TreeNode(None, val, None)
+    def create_tree(self, value):
+        self.root = TreeNode(value)
 
-    def is_empty(self):
-        return not self.head
-
-    def insert_node(self, val):
-        temp = self.head
-        while temp is not temp.is_leaf():
-            if val <= temp.val:
-                temp = temp.left
-            else:
-                temp = temp.right
+    def insert_node(self, value):
+        if self.root is None:
+            self.create_tree(value)
+        else:
+            curr = self.root
+            while curr:
+                if value <= curr.data:  # go left
+                    if curr.left:
+                        curr = curr.left
+                    else:
+                        curr.left = TreeNode(value)
+                        break
+                else:
+                    if curr.right:
+                        curr = curr.right
+                    else:
+                        curr.right = TreeNode(value)
+                        break
 
     def print_tree(self):
-        print(self.head.val)
-        if self.head.left_node:
-            self.head.left_node.print_tree()
-        if self.head.right_node:
-            self.head.right_node.print_tree()
+        if self.root is None:
+            print("Tree is empty")
+        else:
+            print(self.print_tree_recursive(self.root))
+
+    def print_tree_recursive(self, curr):
+        if curr is None:
+            return ""
+        else:
+            return self.print_tree_recursive(curr.left) + " " + str(curr.data) + " " + self.print_tree_recursive(curr.right)
 
 
 def main():
     tree = Tree()
-    with open('input.txt', 'r') as f:   # TODO
-        for val in f.readlines():
-            if tree.is_empty():
-                tree.create_tree(int(val))
-            else:
-                tree.insert_node(int(val))
+    nodes = [1, 9, 7, 2, 5, 6, 11]
+    for node in nodes:
+        tree.insert_node(node)
     tree.print_tree()
 
 
